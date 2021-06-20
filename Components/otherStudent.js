377e1207bee6613fa9578e66e2541b32ac07e297
Kumar <amit.kumar7@hpe.com>
2021-06-20 21:22:23 +0530

    added validation

--- Components/otherStudent.js
+++ Components/otherStudent.js
@@ -14,27 +14,19 @@
 // });
 
 function validateForm() {
-  let dropdownValue="";
-  const element = document.getElementById("selectCourse");
-  element.addEventListener("change", e => {
-    dropdownValue = e.target.value;
-  })
+  // let dropdownValue="";
+  // const element = document.getElementById("selectCourse");
+  // element.addEventListener("change", e => {
+  //   dropdownValue = e.target.value;
+  // })
 
-    let name= document.forms["vnsForm"]["name"].value;
+let name= document.forms["vnsForm"]["name"].value;
 let fathersName= document.forms["vnsForm"]["fathersName"].value;
 let number= document.forms["vnsForm"]["number"].value;
 let amount= document.forms["vnsForm"]["amount"].value;
 let purpose= document.forms["vnsForm"]["purpose"].value;
-    if (name == "" || fathersName=="" || number=="" || dropdown=="" || amount=="" || purpose=="" ||dropdownValue) {
-      alert("Name must be filled out");
+    if (name == "" || fathersName=="" || number=="" || dropdown=="" || amount=="" || purpose=="") {
+      alert("Please fill the require fields");
       return false;
-    }else{
-      console.log(name)
-      console.log(fathersName)
-      console.log(number)
-      console.log(amount)
-      console.log(purpose)
-      console.log(dropdownValue)
-      return true
     }
   }
